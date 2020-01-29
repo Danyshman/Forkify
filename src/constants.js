93eb3561db1289cdfc6fcf19f717c6b25ffd7ada
danyshman <danyshman.azamatov@gmail.com>
2020-01-29 08:31:36 +0300

    add api IMG_URL const

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+const API_KEY = 'c772090e9466428483b9c61a988c8a03';
+const PROXY = 'https://cors-anywhere.herokuapp.com/';
+const IMG_URL = 'https://spoonacular.com/recipeImages';
+
+export { API_KEY };
+export { PROXY };
+export { IMG_URL };
