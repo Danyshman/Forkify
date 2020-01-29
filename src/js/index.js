71dabd509258178b00ac491acd9ba11be302c5c2
danyshman <danyshman.azamatov@gmail.com>
2020-01-29 08:34:24 +0300

    little small changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,26 @@
-// https://api.spoonacular.com/recipes/search
-import axios from 'axios';
+import Search from './models/Search';
+import elements from './views/base';
+import * as searchView from './views/searchView';
 
-async function getResults(query) {
-	const proxy = 'https://cors-anywhere.herokuapp.com/';
-	const key = 'c772090e9466428483b9c61a988c8a03';
-	console.log('start');
-	const result = await axios(`${proxy}https://api.spoonacular.com/recipes/search?apiKey=${key}&query=${query}`);
-	console.log(result);
-	console.log('end');
+/* Global state of the APP
+ * - Search Object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+
+const state = {};
+
+elements.searchForm.addEventListener('submit', controlSearch);
+
+async function controlSearch(event) {
+	event.preventDefault();
+	const query = searchView.getInput();
+	if (query) {
+		state.search = new Search(query);
+		searchView.clearInpurt();
+		searchView.clearRecipeList();
+		await state.search.getRecipes();
+		searchView.showRecipes(state.search.recipes);
+	}
 }
-
-getResults('pizza');
