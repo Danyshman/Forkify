527190000c22458b01b8d6e05dd18321f50e972d
danyshman <danyshman.azamatov@gmail.com>
2020-01-29 08:33:53 +0300

    collect all html element classes in base.js

---
+++ src/js/views/base.js
@@ -0,0 +1,7 @@
+const elements = {
+	searchInput: document.querySelector('.search__field'),
+	searchForm: document.querySelector('.search'),
+	recipesList: document.querySelector('.results__list'),
+};
+
+export default elements;
